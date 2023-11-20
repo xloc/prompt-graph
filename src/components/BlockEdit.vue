db49555c75431338df64cf79024cbfcff7f8b00f
xloc <xloc.cc@outlook.com>
2023-11-19 22:51:10 -0800

    edit page adjustments

--- src/components/BlockEdit.vue
+++ src/components/BlockEdit.vue
@@ -1,17 +1,20 @@
 
 <template>
-  <div class="absolute inset-10 shadow-2xl border rounded-lg p-10 bg-white bg-opacity-80 backdrop-blur-sm">
-    <div class="h-full w-full relative">
+  <div class="absolute inset-0 shadow-2xl backdrop-blur-sm bg-gray-800 bg-opacity-20 p-10">
+    <div class="h-full w-full relative rounded-lg p-10 bg-white bg-opacity-80 ">
       <div class="h-full flex flex-col justify-start items-start">
-        <label class="label">TITLE</label>
-        <input type="text"
-          class="text-2xl field"
-          v-model="block.title" />
-        <label class="label">ID</label>
-        <input type="text" class="text-md field" v-model="block.id" />
-        <label class="label">PROMPT</label>
-        <textarea class="flex-1 h-40 resize-none field"
-          v-model="block.prompt"></textarea>
+        <div class="w-full h-fit relative">
+          <label class="label">TITLE</label>
+          <input type="text" class="field text-2xl" v-model="block.title" />
+        </div>
+        <div class="w-full h-fit">
+          <label class="label">ID</label>
+          <input type="text" class="field text-md" v-model="block.id" />
+        </div>
+        <div class="w-full h-fit flex-1 flex flex-col">
+          <label class="label">PROMPT</label>
+          <textarea class="field flex-1 resize-none " v-model="block.prompt"></textarea>
+        </div>
       </div>
       <div class="absolute bottom-0 right-0 flex justify-end w-full p-2 px-2 text-sm">
         <button class="text-violet-500 p-1 px-2 rounded-sm hover:bg-gray-200"
@@ -51,10 +54,11 @@ onUnmounted(() => {
 
 <style scoped>
 .field {
-  @apply py-1 bg-transparent w-full z-0 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500
+  @apply py-1 bg-transparent w-full rounded-sm;
+  @apply focus:outline-none focus:ring-2 focus:ring-violet-400 z-0;
 }
 
 .label {
-  @apply text-sm text-gray-400 -mb-2 z-10 bg-white mt-2
+  @apply text-sm text-gray-400 -mb-2 z-10 mt-2 block w-fit px-1 -mx-1 relative;
 }
 </style>
