184546b29e24246bd273bc459e77c029a0fd27d6
xloc <xloc.cc@outlook.com>
2023-11-20 20:10:29 -0800

    consistent form style, solve form overflow

--- src/components/BlockEdit.vue
+++ src/components/BlockEdit.vue
@@ -1,26 +1,35 @@
 
 <template>
-  <div class="absolute inset-0 shadow-2xl backdrop-blur-sm bg-gray-800 bg-opacity-20 p-10">
-    <div class="h-full w-full relative rounded-lg p-10 bg-white bg-opacity-80 ">
-      <div class="h-full flex flex-col justify-start items-start">
+  <div class="absolute inset-0 
+    shadow-2xl backdrop-blur-sm bg-gray-800 bg-opacity-20 px-10 
+    overflow-y-auto flex flex-col">
+    <div class="h-10 flex-none"></div>
+
+
+    <div class="h-full w-full relative rounded-lg p-10 bg-white">
+      <div class="h-full flex flex-col justify-start items-start gap-2">
         <div class="w-full h-fit relative">
           <label class="label">TITLE</label>
-          <input type="text" class="field text-2xl" v-model="block.title" />
+          <input type="text" class="field" v-model="block.title" />
         </div>
         <div class="w-full h-fit">
           <label class="label">ID</label>
-          <input type="text" class="field text-md" v-model="block.id" />
+          <input type="text" class="field" v-model="block.id" />
         </div>
         <div class="w-full h-fit flex-1 flex flex-col">
           <label class="label">PROMPT</label>
           <textarea class="field flex-1 resize-none " v-model="block.prompt"></textarea>
         </div>
       </div>
+
       <button @click="emit('close')"
         class="absolute top-5 right-5 flex text-sm text-violet-500 p-1 px-2 hover:bg-gray-200">
         ESC
       </button>
+
+
     </div>
+    <div class="h-10 flex-none"></div>
   </div>
 </template>
 
@@ -46,11 +55,10 @@ useKeyPress('Escape', () => emit('close'));
 
 <style scoped>
 .field {
-  @apply py-1 bg-transparent w-full rounded-sm;
-  @apply focus:outline-none focus:ring-2 focus:ring-violet-400 z-0;
+  @apply w-full border rounded-sm p-2 focus:outline focus:outline-2 focus:outline-violet-400;
 }
 
 .label {
-  @apply text-sm text-gray-400 -mb-2 z-10 mt-2 block w-fit px-1 -mx-1 relative;
+  @apply pl-2 text-gray-500 text-sm;
 }
 </style>
