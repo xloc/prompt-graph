702476e1c119009abdbd0759d6c69d0105968d52
xloc <xloc.cc@outlook.com>
2023-11-28 01:50:26 -0800

    add demo-graph when empty

--- src/models/file-db.ts
+++ src/models/file-db.ts
@@ -49,8 +49,48 @@ export const createNewFile = async (): Promise<GraphFile> => {
   return file!;
 }
 
+
+const DEMO_GRAPH_YAML = `
+- xyhw:
+    x: 148
+    y: 121
+    width: 200
+    height: 150
+  id: a
+  prompt: standalone
+  title: Title goes here
+- xyhw:
+    x: 383
+    y: 400
+    width: 200
+    height: 150
+  id: b
+  prompt: depending on {a}
+  title: Title goes here
+- xyhw:
+    x: 683
+    y: 213
+    width: 200
+    height: 150
+  id: c
+  prompt: depending on {a} and {b}
+  title: Title goes here`.trim();
+
+
 export const EDITING_FILE_PRIMARY_KEY = 'editing-file-id';
 export const getEditingFile = async (): Promise<GraphFile> => {
+  // FIXME: hardcoded demo graph
+  if (await db.files.count() === 0) {
+    const key = await db.files.add({
+      fileName: 'Demo',
+      createAt: new Date(),
+      updateAt: new Date(),
+      content: DEMO_GRAPH_YAML,
+    });
+    const file = await db.files.get(key)
+    return file!;
+  }
+
   // if editing file is in local storage and valid, return it
   const getExistingFile = async () => {
     const key = localStorage.getItem(EDITING_FILE_PRIMARY_KEY);
