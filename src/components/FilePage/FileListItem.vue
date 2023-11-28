0ee91bfa3a2ee67f9bc892d55d45ca48d9164387
xloc <xloc.cc@outlook.com>
2023-11-28 01:16:09 -0800

    better time format

--- src/components/FilePage/FileListItem.vue
+++ src/components/FilePage/FileListItem.vue
@@ -3,7 +3,7 @@
     <template v-if="!isEditingName">
       <div class="overflow-hidden text-ellipsis">{{ file.fileName }}</div>
       <div class="overflow-hidden text-ellipsis | text-sm text-gray-500">
-        {{ file.createAt.toLocaleString() }}</div>
+        {{ updateTime }}</div>
       <div class="absolute inset-0 flex justify-end items-center p-3 gap-2">
         <button @click.stop="isEditingName = true; nextTick().then(() => inputRef!.focus())"
           class="invisible group-hover:visible backdrop-blur-sm 
@@ -32,7 +32,8 @@
 import TrashIcon from '@heroicons/vue/24/outline/TrashIcon';
 import PencilSquareIcon from '@heroicons/vue/24/outline/PencilSquareIcon';
 import { GraphFile } from '../../models/file-db';
-import { nextTick, ref } from 'vue';
+import { computed, nextTick, ref } from 'vue';
+import { format, formatRelative, isPast, isAfter, sub } from 'date-fns';
 
 
 const props = defineProps<{
@@ -47,4 +48,13 @@ const emit = defineEmits<{
 const isEditingName = ref(false);
 const editingFileName = ref(props.file.fileName);
 const inputRef = ref<HTMLInputElement>();
+const updateTime = computed(() => {
+  const time = props.file.updateAt;
+  // if is less than 1 day before, show relative time
+  if (isPast(time) && isAfter(time, sub(Date.now(), { days: 1 }))) {
+    return 'updated ' + formatRelative(time, Date.now());
+  } else {
+    return 'updated at ' + format(time, 'yyyy-MM-dd HH:mm:ss');
+  }
+})
 </script>
